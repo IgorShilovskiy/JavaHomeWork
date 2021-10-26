249b41eb5d41195499252249aa2d70055dab7497
Igor Shilovskiy <dn161280siv@gmail.com>
2021-10-26 19:25:52 +0300

    init commit

--- src/com/pb/shilovskiy/hw4/Anagram.java
+++ src/com/pb/shilovskiy/hw4/Anagram.java
@@ -1,5 +1,6 @@
 package com.pb.shilovskiy.hw4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -11,7 +12,30 @@ import java.util.Scanner;
 
 public class Anagram {
 
-    //проверка на совпадение символов с учетом вхождения нескольких символов не 1 раз
+// Обработка строки - удаление пунктуации и пробелов и приведение к нижнему регистру
+    public static String procWorkStr(String strIn){
+        String resultString;
+        strIn=strIn.toLowerCase();
+        resultString = strIn.replaceAll("\\p{Punct}", "");
+        resultString = resultString.replaceAll(" ", "");
+        return resultString;
+    }
+
+//Сортировка строки
+    public static String Sortstring(String strIn){
+        String strOut="";
+        char [] charStr = strIn.toCharArray();
+        Arrays.sort(charStr);
+
+        System.out.println("The sorted array is:");
+        for (char value : charStr) {
+            strOut=strOut + Character.toString(value);
+        }
+        return strOut;
+    }
+
+
+//проверка на совпадение символов с учетом вхождения нескольких символов не 1 раз
     public static boolean procAnagramm(String s1, String s2) {
         boolean [] arrayf1 = new boolean [s1.length()];
         boolean [] arrayf2 = new boolean [s2.length()];
@@ -58,30 +82,45 @@ public class Anagram {
         System.out.println("Анаграмма");
         System.out.println("Введите первую строку");
         str1 = Scan.nextLine();
-        String[] strOut1 = str1.split(" ");
         System.out.println("Введите вторую строку");
-        str2 = Scan.nextLine();
-        String[] strOut2 = str2.split(" ");
-        // проверяем количество слов
-        if (strOut1.length != strOut2.length) {
-            System.out.println("Введенные Вами строки не являются анаграммой");
-        } else {
-            for (int i = 0; i < strOut1.length; i++) {
-                if (flag != false) {
-                    flag=procAnagramm(strOut1[i], strOut2[i]);
-                } else {
-                    break;
-                }
-
-            }
-            // проверяем итоговый результат сравления строк
-            if (flag == true) {
-                System.out.println("Введенные Вами строки являются анаграммой");
-            }
-            else{
-                System.out.println("Введенные Вами строки не являются анаграммой");
-            }
+          str2 = Scan.nextLine();
+// Берем символы из строки 1 и сравниваем со строкой 2
+        str1=procWorkStr(str1);
+        str1=Sortstring(str1);
+        str2=procWorkStr(str2);
+        str2=Sortstring(str2);
+        if (str1.equals(str2)){
+            System.out.println("Введенные Вами строки являются анаграммой");
         }
+        else{
+            System.out.println("Введенные Вами строки не являются анаграммой");
+        }
+
+// Теперь вариант посложнее (но он был первым подходом, глупо недоделать) - анаграмма по словам
+        //        String[] strOut1 = str1.split(" ");
+//        String[] strOut2 = str2.split(" ");
+        // проверяем количество слов
+//        if (strOut1.length != strOut2.length) {
+//            System.out.println("Введенные Вами строки не являются анаграммой");
+//        } else {
+//            for (int i = 0; i < strOut1.length; i++) {
+//                if (flag != false) {
+//                    flag=procAnagramm(strOut1[i], strOut2[i]);
+//                } else {
+//                    break;
+//                }
+//
+//            }
+//            // проверяем итоговый результат сравления строк
+//            if (flag == true) {
+//                System.out.println("Введенные Вами строки являются анаграммой");
+//            }
+//            else{
+//                System.out.println("Введенные Вами строки не являются анаграммой");
+//            }
+//        }
+
+
 
     }
 }
